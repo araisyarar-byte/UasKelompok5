4fce513fdc2eb8314987f1cce2c3bc8a3728b83b
Nur Hasanah <nurhasanaana152-arch@users.noreply.github.com>
2026-07-26 12:12:38 +0700

    Menambahkan fitur seleksi dan perubahan status pelamar

--- hireflow_seleksi_status_pelamar/seleksi_pelamar/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'seleksi_pelamar'
-urlpatterns = [
-    path('', views.daftar_pelamar, name='daftar'),
-    path('pelamar/<int:pk>/', views.detail_pelamar, name='detail'),
-]
